30646f96e2328c381cb6110fae20ceda719beef5
roginand <roginandv@gmail.com>
2025-10-05 13:54:15 +0800

    feat(auth): add phone number input screen with navigation

---
+++ client-side/app/auth/PhoneNumberPage.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/Button";
+import PhoneInput from "@/components/PhoneInput";
+import AuthLeftButton from "@/components/svg/AuthLeftButton";
+import { useNavigation } from "expo-router";
+import { View, Text } from "react-native";
+
+const PhoneNumberPage = () => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#FFFFFF",
+        paddingTop: 50,
+        paddingHorizontal: 20,
+      }}
+    >
+      <AuthLeftButton onPress={() => navigation.goBack()} />
+
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 12,
+          marginTop: 30,
+        }}
+      >
+        <Text style={{ fontSize: 28, fontWeight: "bold", color: "#000" }}>
+          Enter your phone number
+        </Text>
+        <Text style={{ fontSize: 16, fontWeight: "400", color: "#666" }}>
+          You will receive a code to confirm your identity
+        </Text>
+      </View>
+
+      <View style={{ marginTop: 40 }}>
+        <PhoneInput />
+      </View>
+
+      <View style={{ marginTop: 55 }}>
+        <Button
+          title="Continue"
+          onPress={() => {
+            navigation.navigate("VerifyPhoneNumber" as never);
+          }}
+          backgroundColor="#545EE1"
+          textColor="#fff"
+          borderColor="#545EE1"
+          borderRadius={30}
+          padding={15}
+          width="100%"
+          height={56}
+          margin={0}
+          fontWeight="bold"
+          fontSize={18}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default PhoneNumberPage;
