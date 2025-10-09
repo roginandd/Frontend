4f1f67f3188e85f8c38b4ec8b5ea4e172c6dfe76
roginand <roginandv@gmail.com>
2025-10-09 16:31:34 +0800

    feat(interfaces): added interfaces for order objects

---
+++ client-side/types/interfaces.tsx
@@ -0,0 +1,91 @@
+interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+interface CommissionData {
+  address: string;
+  specification: string;
+  deliveryInstructions: string;
+  coordinates: Coordinates;
+}
+
+export interface GeoapifyFeatureCollection {
+  type: "FeatureCollection";
+  features: GeoapifyFeature[];
+}
+
+export interface GeoapifyFeature {
+  type: "Feature";
+  properties: GeoapifyProperties;
+  geometry: GeoapifyGeometry;
+  bbox?: number[];
+}
+export interface NavigateRegion {
+  Orders: string;
+  address: string;
+  region: any;
+}
+
+export interface GeoapifyProperties {
+  country: string;
+  country_code: string;
+  region?: string;
+  state?: string;
+  city?: string;
+  village?: string;
+  municipality?: string;
+  county?: string;
+  postcode?: string;
+  suburb?: string;
+  iso3166_2?: string;
+  datasource: GeoapifyDatasource;
+  lon: number;
+  lat: number;
+  population?: number;
+  result_type: string;
+  formatted: string;
+  address_line1?: string;
+  address_line2?: string;
+  category?: string;
+  timezone?: GeoapifyTimezone;
+  plus_code?: string;
+  plus_code_short?: string;
+  rank?: GeoapifyRank;
+  place_id: string;
+}
+
+export interface GeoapifyDatasource {
+  sourcename: string;
+  attribution: string;
+  license: string;
+  url: string;
+}
+
+export interface GeoapifyTimezone {
+  name: string;
+  offset_STD: string;
+  offset_STD_seconds: number;
+  offset_DST: string;
+  offset_DST_seconds: number;
+  abbreviation_STD: string;
+  abbreviation_DST: string;
+}
+
+export interface GeoapifyRank {
+  confidence: number;
+  confidence_city_level?: number;
+  match_type?: string;
+}
+
+export interface GeoapifyGeometry {
+  type: "Point";
+  coordinates: [number, number]; // [longitude, latitude]
+}
+
+export interface LocationPickerParams {
+  returnAddress: string;
+  returnLocation: Coordinates;
+}
+
+export { Coordinates, CommissionData };
