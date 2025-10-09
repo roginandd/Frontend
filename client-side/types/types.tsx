1fec9713134ef9fa95897fa8d5e8fe0eddf44670
roginand <roginandv@gmail.com>
2025-10-09 16:31:43 +0800

    feat(types): added types

--- client-side/types/types.tsx
+++ client-side/types/types.tsx
@@ -1,4 +1,7 @@
 import { DimensionValue } from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RouteProp } from "@react-navigation/native";
+import { Coordinates, LocationPickerParams } from "./interfaces";
 
 export type ButtonProps = {
   title: string;
@@ -35,3 +38,47 @@ export type AuthLeftButtonProps = {
   size?: number;
   color?: string;
 };
+
+export type RootStackParamList = {
+  GetStarted: undefined;
+  Welcome: undefined;
+  PhoneNumber: undefined;
+  VerifyPhoneNumber: undefined;
+  VerifyEmailAddress: undefined;
+  PersonalInformation: undefined;
+  StudentIdVerify: undefined;
+  InsuranceVerification: undefined;
+  ApplicationSuccessful: undefined;
+  LoginScreen: undefined;
+  ForgotPasswordScreen: undefined;
+  VerifyEmail: undefined;
+  AddNameScreen: undefined;
+  VeryifyingAccount: undefined;
+  CourierNavigationBar: undefined;
+  Home: undefined;
+  Orders:
+    | {
+        returnAddress: string;
+        returnLocation: Coordinates;
+      }
+    | undefined; // allow it to be optional  LocationPicker: undefined;
+  LocationPicker: {
+    returnAddress: string;
+    returnLocation: Coordinates;
+  };
+};
+
+export type LocationPickerNavProp =
+  NativeStackNavigationProp<RootStackParamList>;
+
+export type OrdersNavProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Orders"
+>;
+
+export type OrdersRouteProp = RouteProp<RootStackParamList, "Orders">;
+
+export type LocationPickerRouteProp = RouteProp<
+  RootStackParamList,
+  "LocationPicker"
+>;
